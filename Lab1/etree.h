84d1a2eba840a2d5ea649cac22f507bfc285837d
Taylor Dodson <t.raynedod@gmail.com>
2017-10-10 11:45:22 -0600

    Added essential structures and functions to header

--- Lab1/etree.h
+++ Lab1/etree.h
@@ -1,2 +1,27 @@
 #ifndef etree_included /*Prevent multiple inclusion. From DEMO code in llist.h*/
 #define etree_included
+
+/*a leaf in a tree.*/
+typedef struct ELeaf {
+  struct ELeaf *left;
+  struct Elead *right;
+  string *name;
+}
+
+  typedef struct {
+    ELeaf *root;
+  } ETree;
+
+/*Create a new tree.*/
+ETree *etAlloc();
+
+/*Free up memory associated with a tree.*/
+void etFree(ETree *lp);
+
+/*Add the given string to the tree.*/
+void etAdd(ETree *tree, string *s);
+
+/*Print the tree. Default message if null.*/
+void etPrint(ETree *tree, string *msg);
+
+#endif
